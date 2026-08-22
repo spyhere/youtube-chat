328b850a5864e6834de3e287e797f899013b5761
spyhere <gekav3@mail.ru>
2026-08-22 03:51:26 +0300

    Create chat header layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
+import { Chat } from "./chat";
+
 export function App() {
   return (
-    <div class="w-screen h-screen bg-stone-300">
+    <div class="w-screen h-screen bg-stone-50 flex items-center justify-center">
+      <Chat />
     </div>
   )
 }
