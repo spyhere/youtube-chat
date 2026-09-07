89b5157943a6b1fc67236816114ba12f0b9fe81c
spyhere <gekav3@mail.ru>
2026-09-07 03:14:36 +0300

    Add "Scroll to bottom" button

--- src/chat/body.tsx
+++ src/chat/body.tsx
@@ -65,6 +65,10 @@ export function Body(props: Props) {
     return true
   }
 
+  const handleFollowChatClick = () => {
+    virtualizer.scrollToEnd({ behavior: "smooth" })
+  }
+
   const [isFollowing, setIsFollowing] = createSignal(true)
 
   onMount(() => {
@@ -130,6 +134,13 @@ export function Body(props: Props) {
           </For>
         </div>
       </div>
+      <button
+        onClick={handleFollowChatClick}
+        class="absolute bottom-0 p-1 bg-blue-500 shadow-md/20 mb-2 rounded-[50%] fill-white cursor-pointer left-1/2 -translate-x-1/2 transition-transform ease-out delay-200 duration-100"
+        style={!isFollowing() ? { transform: "translateY(0)" } : { transform: "translateY(40px)" }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" aria-hidden="true" style="pointer-events: none; display: inherit; width: 100%; height: 100%;"><path d="M12 3a1 1 0 00-1 1v13.586l-5.293-5.293a1 1 0 10-1.414 1.414L12 21.414l7.707-7.707a1 1 0 10-1.414-1.414L13 17.586V4a1 1 0 00-1-1Z"></path></svg>
+      </button>
     </div>
   )
 }
